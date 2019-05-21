8e16b213c9895a9d5167bcfbec1715ab16b062b7
Filip Ruzicka <Filip.Ruzicka@firma.seznam.cz>
2019-05-21 12:39:08 +0200

    Add failing test example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,19 @@
-describe('page', () => {
-  it('works', () => {})
+describe('Cypress doesn\'t see XHRs with URLs ending with ".html"', () => {
+  it('works as expected', () => {
+    cy.server()
+    cy.route('http://example.com/*').as('request')
+    cy.visit('passing.html')
+    cy.wait('@request').then(xhr => {
+      expect(xhr.url).to.equal('http://example.com/passing.htm')
+    })
+  })
+
+  it('doesn\'t work as expected', () => {
+    cy.server()
+    cy.route('http://example.com/*').as('request')
+    cy.visit('failing.html')
+    cy.wait('@request').then(xhr => {
+      expect(xhr.url).to.equal('http://example.com/failing.html')
+    })
+  })
 })
